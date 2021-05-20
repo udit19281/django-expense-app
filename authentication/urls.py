d170b23ce17461937b4c2f25e2ecedc56aaac62e
Udit Bhati <55918713+udit19281@users.noreply.github.com>
2021-05-21 04:36:32 +0530

    login/logout/signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,8 +5,11 @@ from django.views.decorators.csrf import csrf_exempt
 app_name='auth'
 
 urlpatterns = [
-    path('login/',views.login, name='login'),
+    path('login/',views.Login.as_view(), name='login'),
+    path('logout/',views.Logout.as_view(), name='logout'),
     path('signup/',views.signup, name='signup'),
     path('reset/',views.reset, name='reset'),
     path('valid-email/',csrf_exempt(views.ValidateEmail.as_view())),
+    path('valid-username/',csrf_exempt(views.ValidateUsername.as_view())),
+    path('activate/<uid>/<token>',views.activate.as_view(),name="activate")
 ]
