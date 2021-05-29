078bc1119f868a9beb173dbbcfdfb27ceaa0bc94
udit19281 <udit19281@iiitd.ac.in>
2021-05-30 04:56:30 +0530

    CRUD Expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('expenses/',views.expenses, name='expenses'),
     path('expenses/add/',views.expensesadd, name='add'),
     path('expenses/edit/<int:id>',views.expensesedit, name='edit'),
+    path('expenses/delete/<int:id>',views.expensesdelete, name='delete'),
     path('settings/',views.settings, name='settings')
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 from django.contrib.auth.decorators import login_required
 import os
 from django.conf import settings as s
 import json
 from django.contrib import messages
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -12,7 +13,13 @@ def index(request):
 
 @login_required(login_url='/auth/login/')
 def expenses(request):
-    return render(request,'expenses.html')
+    data=Expenses.objects.filter(owner=request.user)
+    pref=Userpref.objects.get(username=request.user)
+    paginator = Paginator(data, 5)
+    page_number = request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    context={'pref':pref,'data':data,'page_obj': page_obj}
+    return render(request,'expenses.html',context=context)
 
 @login_required(login_url='/auth/login/')
 def settings(request):
@@ -52,6 +59,7 @@ def settings(request):
         return render(request,'index.html')
 
 
+@login_required(login_url='/auth/login/')
 def expensesadd(request):
     if request.method == 'POST':
         user=request.user
@@ -64,13 +72,38 @@ def expensesadd(request):
         ent=Expenses.objects.create(owner=user, amount=amount, category=category, date=date, description=description)
         ent.save()
         messages.success(request,"New record added successfully")
-        return render(request,'index.html')
+        return redirect('expenses:expenses')
     category=Category.objects.all()
     context={
         'category':category
     }
+    
     return render(request,'expensesadd.html',context)
 
-
+@login_required(login_url='/auth/login/')
 def expensesedit(request,id):
-    pass
+    if request.method=='GET':
+        ele=Expenses.objects.get(id=id)
+        context={
+            'edit':True,
+            'ele':ele,
+            'category':Category.objects.all()
+        }
+        return render(request,'expensesadd.html',context)
+
+    if request.method=='POST':
+        ele=Expenses.objects.get(id=id)
+        ele.amount=request.POST['amount']
+        ele.category=request.POST['category']
+        ele.date=request.POST['date']
+        ele.description=request.POST['description']
+        ele.save()
+        messages.success(request,"Editted successfully")
+        return redirect('expenses:expenses')
+
+@login_required(login_url='/auth/login/')
+def expensesdelete(request,id):
+    ele=Expenses.objects.get(id=id)
+    ele.delete()
+    messages.success(request,"Deleted Successfull")
+    return redirect('expenses:expenses')
