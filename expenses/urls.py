c81aa5c2e8ceae789776752e4d98f91e449be768
udit19281 <udit19281@iiitd.ac.in>
2021-05-30 07:19:24 +0530

    API and charts

--- expenses/urls.py
+++ expenses/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('expenses/add/',views.expensesadd, name='add'),
     path('expenses/edit/<int:id>',views.expensesedit, name='edit'),
     path('expenses/delete/<int:id>',views.expensesdelete, name='delete'),
-    path('settings/',views.settings, name='settings')
+    path('settings/',views.settings, name='settings'),
+    path('stat/',views.stat, name='stat'),
+    path('expenses/statapi/',views.stat_api, name='statapi'),
 ]
